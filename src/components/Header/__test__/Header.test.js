9600d13a75e48c30d656864043748eeb7e8512b5
Vladimir Kalugin <vladimir.kalugin.irl@gmail.com>
2020-02-03 15:40:49 +0000

    week-3 test fix

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -2,16 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../Header';
 import {render, cleanup} from '@testing-library/react';
+import { Router } from 'react-router-dom';
 
-afterEach(cleanup);
+import { initStore } from "../../../store";
+import { Provider } from 'react-redux'
+import { createMemoryHistory } from "history";
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Header changePage={() => {}}></Header>, div);
+it("renders without crashing", () => {
+    const store = initStore();
+    const div = document.createElement("div");
+    const history = createMemoryHistory();
+  
+    ReactDOM.render(
+      <Router history={history}>
+        <Provider store={store}>
+          <Header />
+        </Provider>
+      </Router>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
-})
+  });
 
-it('matches snapshot', () => {
-    const {container} = render(<Header changePage={() => {}}></Header>)
-    expect(container).toMatchSnapshot(); 
-})
+it("matches snapshot", () => {
+  const store = initStore();
+  const history = createMemoryHistory();
+
+  const { container } = render(
+    <Router history={history}>
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    </Router>
+  );
+  expect(container).toMatchSnapshot();
+});
